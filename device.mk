bea46f874b48f09ba5bccf7dee148f97cd921da3
JaswantTeja <sunnyteja201@gmail.com>
2022-10-01 21:57:02 +0530

    fog: switch to Common QTI Vibrator component
    Signed-off-by: Iverz <asterixiverz@gmail.com>

--- device.mk
+++ device.mk
@@ -325,6 +325,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     gps \
     perf \
     telephony \
+    vibrator \
     wfd \
     wlan
     
@@ -409,10 +410,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
-
-# Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
