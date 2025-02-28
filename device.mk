2697b07317061457799af7aaab3b498660a0e36b
Iverz <asterixiverz@gmail.com>
2025-02-28 22:03:21 +0000

    fog: bring back soundtrigger configs
    01-26 18:20:18.734  9598  9598 E HidlServiceManagement: Service android.hardware.soundtrigger@2.3::ISoundTriggerHw/default must be in VINTF manifest in order to register/get.
    01-26 18:20:18.734  9598  9598 E LegacySupport: Could not register service android.hardware.soundtrigger@2.3::ISoundTriggerHw/default (-2147483648).
    01-26 18:20:18.735   519   519 I hwservicemanager: getTransport: Cannot find entry android.hardware.soundtrigger@2.2::ISoundTriggerHw/default in either framework or device VINTF manifest.
    01-26 18:20:18.735  9598  9598 E SoundTriggerHw: couldn't load sound trigger module sound_trigger.primary (No such file or directory)
    01-26 18:20:18.736   519   519 I hwservicemanager: getTransport: Cannot find entry android.hardware.soundtrigger@2.2::ISoundTriggerHw/default in either framework or device VINTF manifest.
    
    Signed-off-by: Iverz <asterixiverz@gmail.com>

--- device.mk
+++ device.mk
@@ -37,6 +37,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
+    android.hardware.soundtrigger@2.3-impl \
     audio.bluetooth.default \
     audio.primary.default \
     libaudio-resampler
